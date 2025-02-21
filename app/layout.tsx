a2019c95713e8d36db657a0ffc68c8b186cebb44
Archit <archit5793@gmail.com>
2025-02-20 23:25:54 -0500

    chore: add next-themes
    support for dark mode based on system themes

--- app/layout.tsx
+++ app/layout.tsx
@@ -27,7 +27,7 @@ export default async function RootLayout({
   const locale = await getLocale();
   const translations = await getMessages();
   return (
-    <html lang={locale}>
+    <html lang={locale} suppressHydrationWarning>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
